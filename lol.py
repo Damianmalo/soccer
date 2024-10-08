0e890d6319a1b4c5f8f087c976e56c198f744bde
Damianmalo <Damian.malo12@gmail.com>
2024-10-08 11:27:42 -0400

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,9 @@
+name = input("Type your name here ")
+he_she = input("Type your prnouns here (he or she) ")
+favorite = input("Who is your favorite soccer player ")
+
+
+print((name)  + " loves " + (favorite) + " but whoever " + (he_she) +" thinks is on top,")
+print("is false because Lionel Andres Messi is the goat, but if you said him already,")
+print("then congratulations! You know footy.") 
+
